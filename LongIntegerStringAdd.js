111758ed5688bf5c24575d872021c7503de4628e
Alexander Li <alex123bob@users.noreply.github.com>
2020-08-08 12:09:53 +0800

    Update LongIntegerStringAdd.js

--- LongIntegerStringAdd.js
+++ LongIntegerStringAdd.js
@@ -22,132 +22,132 @@
 // 输入：add("1234", "-1234")
 // 输出："0"
 
-function add (strFirst, strSecond) {
-	first = strFirst.split('');
-  	second = strSecond.split('');
-  	let carryOver = 0;
-  	let borrow = 0;
-  	let sign = '';
-  
+function add(strFirst, strSecond) {
+    first = strFirst.split('');
+    second = strSecond.split('');
+    let carryOver = 0;
+    let borrow = 0;
+    let sign = '';
+
     /**
      * function instance to do single digit add.
      * @param {String} dA 
      * @param {String} dB 
      */
-  	let singleDigitAdd = function(dA, dB) {
-		let sum = parseInt(dA, 10) + parseInt(dB, 10) + carryOver;
+    let singleDigitAdd = function (dA, dB) {
+        let sum = parseInt(dA, 10) + parseInt(dB, 10) + carryOver;
         let remainder;
         if (sum > 10) {
-          remainder = sum % 10;
-          carryOver = 1;
-          return remainder;
+            remainder = sum % 10;
+            carryOver = 1;
+            return remainder;
         }
         else {
-          carryOver = 0;
-          return sum;
+            carryOver = 0;
+            return sum;
         }
-	}
-  
+    }
+
     /**
      * Likewise, single digit subtraction.
      * @param {String} dA 
      * @param {String} dB 
      */
-  	let singleDigitSubtract = function (dA, dB) {
-      	let a = parseInt(dA, 10);
-      	let b = parseInt(dB, 10);
-      	let res;
-      	if (a >= b) {
-          res = a - borrow - b;
-          borrow = 0;
+    let singleDigitSubtract = function (dA, dB) {
+        let a = parseInt(dA, 10);
+        let b = parseInt(dB, 10);
+        let res;
+        if (a >= b) {
+            res = a - borrow - b;
+            borrow = 0;
         }
-      	else {
-          res = a + 10 - borrow - b;
-          borrow = 1;
+        else {
+            res = a + 10 - borrow - b;
+            borrow = 1;
         }
-      	return res;
-  	}
-  
+        return res;
+    }
+
     /**
      * Kick "sign" outta the input array, reverse array in order to make it easier to do adding & subtraction.
      * @param {Array<String>} arr 
      */
-  	let sortOut = function (arr) {
-    	return /-/i.test(arr[0]) ? (sign += '-', arr.slice(1).reverse()) : arr.reverse();
+    let sortOut = function (arr) {
+        return /-/i.test(arr[0]) ? (sign += '-', arr.slice(1).reverse()) : arr.reverse();
     }
-  
-  	/**
-       * Assume the operation is subtraction
-       * @param {String} strA 
-       * @param {String} strB 
-       */
-  	let detectResultSign = function(strA, strB) {
-      	return (
-          (strA[0] === '-' && Math.abs(strA) > Math.abs(strB))
-          || (strB[0] === '-' && Math.abs(strB) > Math.abs(strA))
+
+    /**
+   * Assume the operation is subtraction
+   * @param {String} strA 
+   * @param {String} strB 
+   */
+    let detectResultSign = function (strA, strB) {
+        return (
+            (strA[0] === '-' && Math.abs(strA) > Math.abs(strB))
+            || (strB[0] === '-' && Math.abs(strB) > Math.abs(strA))
         ) ? '-' : '';
     }
-  
-  	/**
-       * Check if result array is filled with straight zero.
-       * @param {Array<Number>} arr 
-       */
-  	let isZero = function(arr) {
-    	return arr.reduce(function(pre, current) {
-          return parseInt(pre, 10) + parseInt(current, 10);
+
+    /**
+   * Check if result array is filled with straight zero.
+   * @param {Array<Number>} arr 
+   */
+    let isZero = function (arr) {
+        return arr.reduce(function (pre, current) {
+            return parseInt(pre, 10) + parseInt(current, 10);
         }, 0) === 0;
     }
-  
+
     /**
      * Trim result array that starts with couple of "zero" digits 
      * @param {Array<Number>} arr 
      */
-  	let trimZero = function(arr) {
-		let stop = false;
-      	let trimIdx = -1;
-      	arr.forEach(function(el, idx) {
-          if (stop) {
-            // do nothing.
-          }
-          else {
-            if (el === 0) {
-              trimIdx = idx;
+    let trimZero = function (arr) {
+        let stop = false;
+        let trimIdx = -1;
+        arr.forEach(function (el, idx) {
+            if (stop) {
+                // do nothing.
             }
             else {
-              stop = true;
+                if (el === 0) {
+                    trimIdx = idx;
+                }
+                else {
+                    stop = true;
+                }
             }
-          }
         });
         return trimIdx >= 0 ? arr.slice(trimIdx + 1) : arr;
-	}
-  
-  	first = sortOut(first);
-  	second = sortOut(second);
-  
-  	let isSubtraction = /^-$/i.test(sign);
-  
-  	// Add
-  	if (!isSubtraction) {
-    	let res = first.length > second.length ? first.map(function(firstEl, idx) {
-          	let secondEl = second[idx] || 0;
-          	return singleDigitAdd(firstEl, secondEl);
-          
-        }) : second.map(function(secondEl, idx) {
-          	let firstEl = first[idx] || 0;
-          	return singleDigitAdd(secondEl, firstEl);
+    }
+
+    first = sortOut(first);
+    second = sortOut(second);
+
+    let isSubtraction = /^-$/i.test(sign);
+
+    // Add
+    if (!isSubtraction) {
+        let res = first.length > second.length ? first.map(function (firstEl, idx) {
+            let secondEl = second[idx] || 0;
+            return singleDigitAdd(firstEl, secondEl);
+
+        }) : second.map(function (secondEl, idx) {
+            let firstEl = first[idx] || 0;
+            return singleDigitAdd(secondEl, firstEl);
         })
         return res.reverse().join('');
     }
-  	// Subtraction
-  	else {
-      	let resultSign = detectResultSign(strFirst, strSecond);
-    	let res = first.length > second.length || Math.abs(first) > Math.abs(second) ? first.map(function(firstEl, idx) {
-			let secondEl = second[idx] || 0;
-          	return singleDigitSubtract(firstEl, secondEl);
-		}) : second.map(function(secondEl, idx){
-          	let firstEl = first[idx] || 0;
-          	return singleDigitSubtract(secondEl, firstEl);
-		})
+    // Subtraction
+    else {
+        let resultSign = detectResultSign(strFirst, strSecond);
+        let res = first.length > second.length || Math.abs(first) > Math.abs(second) ? first.map(function (firstEl, idx) {
+            let secondEl = second[idx] || 0;
+            return singleDigitSubtract(firstEl, secondEl);
+        }) : second.map(function (secondEl, idx) {
+            let firstEl = first[idx] || 0;
+            return singleDigitSubtract(secondEl, firstEl);
+        })
         return isZero(res) ? 0 : resultSign + trimZero(res.reverse()).join('');
     }
 }
